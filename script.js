829e2dac51e10607db1c0dcba04a6f92afb7ef25
AroneHaha <165986448+AroneHaha@users.noreply.github.com>
2025-06-23 21:10:09 +0800

    no api yet

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const askBtn = document.getElementById("ask-btn")
+const micTxt = document.getElementById("mic-btn")
 const promptTxt = document.getElementById("text-area")
 
+let answer = 'Hiddwaddwa'
+let long = 'HiddwaddwaHiddwaddwaHiddwaddwaHiddwaddwaHiddwaddwaHiddwaddwaHiddwaddwaHiddwaddwaHiddwaddwaHiddwaddwaHiddwaddwaHiddwaddwaHiddwaddwaHiddwaddwaHiddwaddwaHiddwaddwaHiddwaddwaHiddwaddwaHiddwaddwaHiddwaddwaHiddwaddwaHiddwaddwaHiddwaddwaHiddwaddwaHiddwaddwaHiddwaddwaHiddwaddwa'
+
 promptTxt.addEventListener('keydown', (e) => {
-    if (e.key == 'Enter' && !e.shiftKey && (promptTxt.value.trim() != '')) {
+    if (e.key == 'Enter' && (!e.shiftKey && (promptTxt.value.trim() != ''))) {
         e.preventDefault();
+        showResponse(answer);
+        showResponse(long);
         promptTxt.value = ''
     }
 })
@@ -13,3 +19,23 @@ promptTxt.addEventListener('keydown', (e) => {
         promptTxt.value = ''
     }
 })
+
+askBtn.addEventListener('click', () => {
+    if (promptTxt.value.trim() != '') {
+        showResponse('DAPAT GUMANA TO')
+        promptTxt.value = ''
+    }
+})
+
+// --------------------------------- RESPONSE BOX --------------------------------------------------
+const responseContainer = document.getElementById('response-container');
+function showResponse(text) {
+    const responseDiv = document.createElement('div');
+    responseDiv.classList.add('response');
+    responseDiv.innerText = text;
+    responseContainer.appendChild(responseDiv);
+    responseContainer.scrollTop = responseContainer.scrollHeight;
+}
+
+showResponse("hi");
+
